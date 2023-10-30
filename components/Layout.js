067664fcc2fdacb556a2b2ed54611113f34fec07
masayakobayashi <msy.kobayashi5401@gmail.com>
2023-10-30 23:03:04 +0900

    特商法ページリンク紐づけ

--- components/Layout.js
+++ components/Layout.js
@@ -36,14 +36,11 @@ export default function Layout({ children, title = "HP by Nextjs" }) {
       <footer className="w-full h-12 flex justify-center items-center border-t">
         <a
           className="flex items-align"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="./tokushohou-page"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span>
-            <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-          </span>
+          特定商取引法に関する情報
         </a>
       </footer>
     </div>
--- pages/contact-page.js
+++ pages/contact-page.js
@@ -9,7 +9,7 @@ const Contact = () => {
         <div className="flex justify-center mt-4">
           <Image
             className="rounded-full"
-            src="/avatar.jpg"
+            src="/7118.jpg"
             width={60}
             height={60}
             alt="Avatar"
@@ -17,11 +17,9 @@ const Contact = () => {
         </div>
         <div className="mt-4">
           <p className="font-bold">Address</p>
-          <p className="text-xs mt-2 text-gray-600">city A</p>
+          <p className="text-xs mt-2 text-gray-600">minato city</p>
           <p className="font-bold mt-3">Email</p>
-          <p className="text-xs mt-2 text-gray-600">abc@gmail.com</p>
-          <p className="font-bold mt-3">Phone</p>
-          <p className="text-xs mt-2 text-gray-600">000-123-456</p>
+          <p className="text-xs mt-2 text-gray-600">m-kobayashi@onecre.net</p>
         </div>
       </div>
     </Layout>
